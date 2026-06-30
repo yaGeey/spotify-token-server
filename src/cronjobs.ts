90ba228e000b04547eb0bfc3bb2606d1c2bee467
yaGeey <krabadaj@gmail.com>
2026-06-30 19:51:35 +0300

    disable cronjobs

--- src/cronjobs.ts
+++ src/cronjobs.ts
@@ -3,22 +3,22 @@ import { queue } from './index.js'
 import { updateAllHashes } from './hashHandlers.js'
 import { handleError, restartBrowser } from './browser.js'
 
-nodeCron.schedule('0 3 * * *', () => {
-   const randomDelay = Math.floor(Math.random() * 1000 * 60 * 60 * 2)
-   setTimeout(async () => {
-      try {
-         // restart browser to prevent memory leaks and other issues
-         await queue.add(async () => await restartBrowser())
+// nodeCron.schedule('0 3 * * *', () => {
+//    const randomDelay = Math.floor(Math.random() * 1000 * 60 * 60 * 2)
+//    setTimeout(async () => {
+//       try {
+//          // restart browser to prevent memory leaks and other issues
+//          await queue.add(async () => await restartBrowser())
 
-         // update hashes 
-         const hashes = await updateAllHashes()
-         if (Object.keys(hashes).some((key) => hashes[key] === null)) {
-            console.error('cron: Failed to update some hashes', hashes)
-         } else {
-            console.log('✅ Cron: All hashes updated successfully')
-         }
-      } catch (err) {
-         handleError(err)
-      }
-   }, randomDelay)
-})
+//          // update hashes 
+//          const hashes = await updateAllHashes()
+//          if (Object.keys(hashes).some((key) => hashes[key] === null)) {
+//             console.error('cron: Failed to update some hashes', hashes)
+//          } else {
+//             console.log('✅ Cron: All hashes updated successfully')
+//          }
+//       } catch (err) {
+//          handleError(err)
+//       }
+//    }, randomDelay)
+// })
